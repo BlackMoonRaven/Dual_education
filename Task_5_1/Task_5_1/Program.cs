cbe9a1487f08af3bc8688aa7f267287fee529553
Andrii Politychka <71886509+Narkokot@users.noreply.github.com>
2021-10-26 00:30:05 +0300

    Add files via upload

---
+++ Task_5_1/Task_5_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_5_1
+{
+
+    //Інтерфейс
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    //Клас пташка
+    public class Bird : IFlyable
+    {
+        public string name { get; set; }
+        public bool canFly { get; set; }
+
+        public void Fly()
+        {
+            if (canFly)
+            {
+                Console.WriteLine($"The {name} can fly");
+            }
+            else
+            {
+                Console.WriteLine($"The {name} cannot fly");
+            }
+        }
+    }
+
+    //клас літак
+    public class Plane : IFlyable
+    {
+        public string mark { get; set; }
+        public int highFly { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"The {mark} can fly up to {highFly} km");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Заповнення списку
+            List<IFlyable> list = new List<IFlyable>
+            {
+                new Bird {name = "Falcon", canFly = true},
+                new Bird {name = "Penguin", canFly = false},
+                new Bird {name = "Eagle", canFly = true},
+
+                new Plane {mark = "F-4", highFly = 30},
+                new Plane {mark = "p-10", highFly = 7},
+                new Plane {mark = "SR-71", highFly = 25}
+            };
+
+            //Вивід
+            foreach (IFlyable info in list)
+            {
+                info.Fly();
+            }
+
+        }            
+    }
+}
+
+
+
---
+++ Task_5_2/Task_5_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+namespace Task_5_2
+{
+    class Program
+    {
+        static void out_(ArrayList myColl)
+        {
+            //Вивід на екран
+            foreach (object i in myColl)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //Список чисел
+            ArrayList myColl = new ArrayList();
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"Input number {i+1}/10: ");
+                myColl.Add(int.Parse(Console.ReadLine()));
+            }
+            out_(myColl);
+
+            //Вивід індекса чисел "-10"
+            for(int i = 0; i < 10; i++)
+            {
+                if((int)myColl[i] == -10)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Видалення елементів які більші за "20"
+            for(int i = 0, j = 10; i < j; i++)
+            {
+                if((int)myColl[i] > 20)
+                {
+                    myColl.Remove(myColl[i]);
+                    j--;
+                    i--;
+                }
+            }
+            out_(myColl);
+
+            //Встівка чисел по індексу
+            myColl.Insert(2, 1);
+            myColl.Insert(8, -3);
+            myColl.Insert(5, -4);
+            out_(myColl);
+
+            //Сортування
+            myColl.Sort();
+            out_(myColl);
+        }
+    }
+}
